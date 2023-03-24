9febbcb5a441afc7aae74d3f531a78aa37df631c
dwiadiningsih <127500971+dwiadiningsih@users.noreply.github.com>
2023-03-24 22:26:09 +0800

    Add files via upload

---
+++ Tugas2_SourceCode/Incr.java
@@ -0,0 +1,16 @@
+/*Nama : Dwi Adiningsih
+Nim : 13020210021
+Hari/tgl : Jumat, 10 Maret 2022
+waktu : 9.09*/
+
+public class Incr {
+	
+	public static void main(String[] args) {
+
+	int i, j;
+
+	i = 3;
+	j = i++;
+	System.out.println ("Nilai i : " + (++i) + "\nNilai j : " + j);
+	}
+}
---
+++ Tugas2_SourceCode/PrintXWhile.java
@@ -0,0 +1,30 @@
+/*	NIM   		: 13020210021
+	Nama   		: Dwi Adiningsih
+	Hari/Tanggal	: Rabu, 22 Maret 2023
+	Waktu Pengerjaan  : 12.15 */
+import java.util.Scanner;
+/* contoh baca nilai x, */
+/* Jumlahkan nilai yang dibaca dengan WHILE */
+public class PrintXWhile {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+/* Kamus : */
+int Sum;
+int x;
+Scanner masukan=new Scanner(System.in);
+/* Program */
+Sum = 0; /* Inisialisasi */
+System.out.print ("Masukkan nilai x (int), akhiri dg 999 : ");
+x = masukan.nextInt();/* First Elmt*/
+while (x != 999) /* Kondisi berhenti */ {
+Sum = Sum + x; /* Proses */
+System.out.print("Masukkan nilai x (int), akhiri dg 999 : ");
+x = masukan.nextInt();/* First Elmt*/
+}
+System.out.println("Hasil penjumlahan = "+ Sum); /*
+Terminasi */
+}
+}
